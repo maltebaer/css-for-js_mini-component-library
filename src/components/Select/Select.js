4ea26924d3c64b7bb49cdaff97ffd13f6e2bd723
malte.baer <malte.baer@biomes.world>
2022-12-15 22:28:42 +0100

    first shot select, focus missing

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -6,13 +6,39 @@ import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
+    const displayedValue = getDisplayedValue(value, children);
 
-  return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
-  );
+    return (
+        <Wrapper>
+            {displayedValue}
+            <IconWrapper>
+                <Icon id="chevron-down" size="16" strokeWidth={2} />
+            </IconWrapper>
+            <StyledSelect value={value} onChange={onChange}>
+                {children}
+            </StyledSelect>
+        </Wrapper>
+    );
 };
+
+const Wrapper = styled.div`
+    position: relative;
+    display: inline-flex;
+    padding: 12px 16px;
+    background-color: ${COLORS.transparentGray15};
+    color: ${COLORS.gray700};
+    border-radius: 8px;
+
+    &:hover {
+        color: ${COLORS.black};
+    }
+`
+const IconWrapper = styled.div`
+    margin-left: 24px;
+`
+const StyledSelect = styled.select`
+    opacity: 0;
+    position: absolute;
+`
 
 export default Select;
